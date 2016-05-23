4c64d46ebea352ec1cfc8e663081ceb9c1592592
bmcleod <lbmcleod@gmail.com>
2016-05-23 11:56:42 -0400

    Initial setup

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bookappApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
