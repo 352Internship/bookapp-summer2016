0d18b00bdc40dda52cf0cc97b8f19743f58e3db2
bmcleod <lbmcleod@gmail.com>
2016-05-23 11:25:29 -0400

    Wiped MEAN.io, installed project via Yeoman.

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bookappApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
