4e3ce1151ffa64275e8f5c73661feac45abb8ad9
jr8000 <josh@Joshs-MacBook-Pro.local>
2016-05-25 11:19:59 -0400

    setup an item model

--- server/api/items/items.model.js
+++ server/api/items/items.model.js
@@ -5,8 +5,13 @@ var mongoose = require('mongoose'),
 
 var ItemsSchema = new Schema({
   name: String,
+  isbn: Number,
   info: String,
-  active: Boolean
+  active: Boolean,
+  type: {
+    type: String, default: "Textbook",
+    enum: ['Textbook', 'Workbook', 'Solution Manual', 'Lab Manual', 'Lab Workbook', 'Study Guide']
+  }
 });
 
 module.exports = mongoose.model('Items', ItemsSchema);
