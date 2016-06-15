7eb5e7a2e023ee4b24324e842db8291f56216fc1
bmcleod <lbmcleod@gmail.com>
2016-06-15 11:13:51 -0400

    mark an item as status false after transaction is started. load fresh items after transaction started

--- client/app/find/find.controller.js
+++ client/app/find/find.controller.js
@@ -1,26 +1,30 @@
 'use strict';
 
 angular.module('bookappApp')
-  .controller('FindCtrl', function ($scope, activeItemsService, User, transactionService) {
+  .controller('FindCtrl', function ($scope, activeItemsService, User, transactionService, itemsService) {
 
     var userEmail,
        userDomain;
 
-    $scope.getCurrentUser = User.get();
+    function loadItemsData() {
+      $scope.getCurrentUser = User.get();
 
-    $scope.getCurrentUser.$promise.then(function(data) {
-      userEmail = data.email.split("@");
-      userDomain = userEmail[userEmail.length - 1];
+      $scope.getCurrentUser.$promise.then(function(data) {
+        userEmail = data.email.split("@");
+        userDomain = userEmail[userEmail.length - 1];
 
-      // Use the itemsService to fetch all items data
-      activeItemsService.find({ domain: userDomain }).$promise.then(function (data) {
-        $scope.items = data;
+        // Use the itemsService to fetch all items data
+        activeItemsService.find({ domain: userDomain }).$promise.then(function (data) {
+          $scope.items = data;
+        });
+
+        $scope.currentDomain = userDomain;
+
+        $scope.currentUserID = data._id;
       });
+    }
 
-      $scope.currentDomain = userDomain;
-
-      $scope.currentUserID = data._id;
-    });
+    loadItemsData(); // get item data on initial load
 
     $scope.message = function (itemid, sellerid) {
       var trans = new transactionService({});
@@ -33,7 +37,14 @@ angular.module('bookappApp')
       }];
 
       trans.$save(function(response){
-    						console.log(response);
+      	console.log(response);
+        if(response._id) {
+          var item = new itemsService({ _id: itemid });
+          item.status = false;
+          item.$update(function (response) {
+            loadItemsData();
+          });
+        }
     	});
     };
 
--- client/components/itemsService/itemsService.service.js
+++ client/components/itemsService/itemsService.service.js
@@ -3,7 +3,12 @@
 angular.module('bookappApp')
   .service('itemsService', function ($resource) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-      return $resource('/api/items/:id', { id: '@_id' });
+      return $resource('/api/items/:id', { id: '@_id' }, {
+        update: {
+          method: 'PUT' // this method issues a PUT request
+        }
+      }
+    )
   })
   .service('activeItemsService', function ($resource) {
     // AngularJS will instantiate a singleton by calling "new" on this function
